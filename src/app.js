a849edb3804b974ea9a2bbd9f6a15c43621a9c8f
wuwenxing <1428560819@qq.com>
2020-01-10 12:13:05 +0800

    添加button group组件

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import Button from './button';
 import Icon from './icon';
+import ButtonGroup from './button-group'
 
 Vue.component('v-button', Button);
 Vue.component('v-icon', Icon);
+Vue.component('v-button-group', ButtonGroup);
 
 new Vue({
   el: '#app',
