ce6d28559064d50850938eb3ebb620dd81954293
wuwenxing <1428560819@qq.com>
2020-01-10 17:10:21 +0800

    单元测试

--- src/app.js
+++ src/app.js
@@ -13,4 +13,94 @@ new Vue({
     loading1: false,
     loading2: true
   }
-})
+});
+
+import chai from 'chai';
+import spies from 'chai-spies';
+chai.use(spies);
+
+const expect = chai.expect;
+
+{
+  const constructor = Vue.extend(Button);
+  const vm = new constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  });
+  vm.$mount();
+  let useElement = vm.$el.querySelector('use');
+  let href = useElement.getAttribute('xlink:href');
+  expect(href).to.eq('#icon-settings');
+  vm.$el.remove();
+  vm.$destroy();
+}
+
+{
+  const constructor = Vue.extend(Button);
+  const vm = new constructor({
+    propsData: {
+      icon: 'settings',
+      loading: true
+    }
+  });
+  vm.$mount();
+  let useElement = vm.$el.querySelector('use');
+  let href = useElement.getAttribute('xlink:href');
+  expect(href).to.eq('#icon-loading');
+  vm.$el.remove();
+  vm.$destroy();
+}
+
+{
+  const div = document.createElement('div');
+  document.body.appendChild(div);
+  const constructor = Vue.extend(Button);
+  const vm = new constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  });
+  vm.$mount(div);
+  let svg = vm.$el.querySelector('svg');
+  let {order} = window.getComputedStyle(svg);
+  expect(order).to.eq('1');
+  vm.$el.remove();
+  vm.$destroy();
+}
+
+{
+  const div = document.createElement('div');
+  document.body.appendChild(div);
+  const constructor = Vue.extend(Button);
+  const vm = new constructor({
+    propsData: {
+      icon: 'settings',
+      iconPosition: 'right'
+    }
+  });
+  vm.$mount(div);
+  let svg = vm.$el.querySelector('svg');
+  let {order} = window.getComputedStyle(svg);
+  expect(order).to.eq('2');
+  vm.$el.remove();
+  vm.$destroy();
+}
+
+{
+  const constructor = Vue.extend(Button);
+  const vm = new constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  });
+  vm.$mount();
+  let spy = chai.spy(function() {});
+  vm.$on('click', spy);
+
+  let button = vm.$el;
+  button.click();
+  expect(spy).to.have.been.called();
+  vm.$el.remove();
+  vm.$destroy();
+}
