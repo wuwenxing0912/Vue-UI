85d937a3d1bb8e85e1101d48e07261c40f4cbe43
xing <1428560819@qq.com>
2020-02-03 14:33:31 +0800

    添加col和row组件

--- src/app.js
+++ src/app.js
@@ -2,12 +2,16 @@ import Vue from 'vue';
 import Button from './button';
 import Icon from './icon';
 import ButtonGroup from './button-group';
-import Input from './input'
+import Input from './input';
+import Row from './row';
+import Col from './col';
 
 Vue.component('v-button', Button);
 Vue.component('v-icon', Icon);
 Vue.component('v-button-group', ButtonGroup);
 Vue.component('v-input', Input);
+Vue.component('v-row', Row);
+Vue.component('v-col', Col);
 
 new Vue({
   el: '#app',
---
+++ src/col.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="col" :class="[`col-${span}`]">
+  <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "VCol",
+    props: {
+      span: {
+        type: [Number, String]
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  .col {
+    height: 100px;
+    width: 50%;
+    background-color: grey;
+    border: 1px solid red;
+  }
+
+  $class: col-;
+  @for $n from 1 through 24 {
+    &.#{$class}#{$n} {
+      width: $n / 24 * 100%;
+    }
+  }
+</style>
