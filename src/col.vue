93921e24f9ff40855eef41d21a8ea379e28990f5
xing <1428560819@qq.com>
2020-02-03 16:08:13 +0800

    添加gutter和offset接口

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
-  <slot></slot>
+  <div class="col" :class="colClasses" :style="colStyle">
+    <div style="border: 1px solid green; height: 50px;">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
@@ -10,6 +12,25 @@
     props: {
       span: {
         type: [Number, String]
+      },
+      offset: {
+        type: [Number, String]
+      }
+    },
+    data() {
+      return {
+        gutter: 0
+      }
+    },
+    computed: {
+      colClasses() {
+        return [this.span && `col-${this.span}`, this.offset && `offset-${this.offset}`];
+      },
+      colStyle() {
+        return {
+          paddingLeft: this.gutter / 2 + 'px',
+          paddingRight: this.gutter / 2 + 'px'
+        }
       }
     }
   }
@@ -17,16 +38,21 @@
 
 <style scoped lang="scss">
   .col {
-    height: 100px;
+    height: 50px;
     width: 50%;
-    background-color: grey;
-    border: 1px solid red;
   }
 
   $class: col-;
   @for $n from 1 through 24 {
     &.#{$class}#{$n} {
       width: $n / 24 * 100%;
+    }
+  }
+
+  $class: offset-;
+  @for $n from 1 through 24 {
+    &.#{$class}#{$n} {
+      margin-left: $n / 24 * 100%;
     }
   }
 </style>
--- src/row.vue
+++ src/row.vue
@@ -1,17 +1,36 @@
 <template>
-  <div class="row">
+  <div class="row" :style="rowStyle">
   <slot></slot>
   </div>
 </template>
 
 <script>
   export default {
-    name: "VRow"
+    name: "VRow",
+    props: {
+      gutter: {
+        type: [Number, String]
+      }
+    },
+    mounted() {
+      this.$children.forEach(vm => {
+        vm.gutter = this.gutter
+      })
+    },
+    computed: {
+      rowStyle() {
+        return {
+          marginLeft: -this.gutter / 2 + 'px',
+          marginRight: -this.gutter / 2 + 'px'
+        }
+      }
+    }
   }
 </script>
 
 <style scoped lang="scss">
   .row {
     display: flex;
+    margin-bottom: 10px;
   }
 </style>
