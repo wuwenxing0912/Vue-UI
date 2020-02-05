b75776088ef7bc872b132fa77f3017b229289a4f
xing <1428560819@qq.com>
2020-02-05 16:11:54 +0800

    解决currentToast的一个 bug

--- src/plugin.js
+++ src/plugin.js
@@ -7,18 +7,25 @@ export  default {
       if (currentToast) {
         currentToast.close();
       }
-      currentToast = createToast({Vue, message, toastOptions});
+      currentToast = createToast({Vue,
+        message,
+        toastOptions,
+        onClose: () => {
+        currentToast = null;
+        }
+      });
     }
   }
 }
 
-function createToast({Vue, message, toastOptions}) {
+function createToast({Vue, message, toastOptions, onClose}) {
   let Constructor = Vue.extend(Toast);
   let toast = new Constructor({
     propsData: toastOptions
   });
   toast.$slots.default = [message];
   toast.$mount();
+  toast.$on('close', onClose);
   document.body.appendChild(toast.$el);
   return toast;
 }
